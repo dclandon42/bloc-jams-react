589551be7960d7fcf055b1b0696a975af37c72d4
Dylan Landon <dylan43landon@gmail.com>
2018-12-21 17:51:28 -0800

    Checkpoint 17 Complete

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
+          <nav className="NavBar">
+            <Link id="link1" to='/'>Landing</Link>
+            <Link id="link2" to='/library'>Library</Link>
           </nav>
-          <h1>Bloc Jams</h1>
+          <h1 id="title">Bloc Jams</h1>
         </header>
         <main>
           <Route exact path ="/" component={Landing} />
--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import albumData from './../data/albums';
 import PlayerBar from './PlayerBar';
+import './Album.css'
 
 class Album extends Component {
   constructor(props){
@@ -148,7 +149,7 @@ class Album extends Component {
             <div id="release-info">{this.state.album.releaseInfo}</div>
             </div>
           </section>
-          <table id="song-list">
+          <table className="song-list">
             <colgroup>
               <col id="song-number-column" />
               <col id="song-title-column" />
